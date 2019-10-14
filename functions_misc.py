20f9fdd03f61556f4173ebbe7235653988e300dc
biojibia <drguggiana@gmail.com>
2019-10-14 14:42:49 +0200

    multid interpolation

--- functions_misc.py
+++ functions_misc.py
@@ -153,3 +153,18 @@ def normalize_matrix(matrix, target=None, axis=None):
         out_matrix = (matrix - np.nanmin(matrix, axis=axis).reshape(-1, 1)) / (
                     np.nanmax(matrix, axis=axis).reshape(-1, 1) - np.nanmin(matrix, axis=axis).reshape(-1, 1))
     return out_matrix
+
+
+def interp_trace_multid(x_known, y_known, x_target):
+    """Interpolate a trace by building an interpolant"""
+    # filter the values so the interpolant is trained only on sorted x points (required by the function)
+    sorted_frames = np.hstack((True, np.invert(x_known[1:] <= x_known[:-1])))
+    x_known = x_known[sorted_frames]
+    y_known = y_known[sorted_frames, :]
+    # also remove any NaN frames
+    notnan = ~np.isnan(np.sum(y_known, axis=1))
+    x_known = x_known[notnan]
+    y_known = y_known[notnan, :].T
+    # create the interpolant
+    interpolant = interp1d(x_known, y_known, kind='linear', bounds_error=False, fill_value=np.nanmean(y_known, axis=1))
+    return interpolant(x_target).T
