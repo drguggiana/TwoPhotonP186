cd8e1cdaf94f7e8dfe4c53cfde83cfe293fd6f68
mkm4884 <matthew.mccann13@gmail.com>
2019-10-25 12:26:10 +0200

    Bugfixes anf progress bar in bin_to_h5.py, bug fixes to interp trace in functions_misc.py, bugfixes in exception cases of file_combine.py

--- bin_to_h5.py
+++ bin_to_h5.py
@@ -9,14 +9,27 @@ from functions_misc import multifolder
 # load the target folders (select the experiment folders containing the files
 file_path = multifolder(master_path)
 
+# flag if we want to make a small test file - MM 17 Oct 2019
+make_test = 0
+if make_test:
+    print("Creating a reduced file for suite2p training.")
+
 # for all the selected files
 for folders in file_path:
     # get the names of the bin files
     bin_files = [join(folders, el) for el in listdir(folders) if el.endswith('.bin')]
     green_path = bin_files[0]
     red_path = bin_files[1]
+
     # define the output path
-    out_path = join(folders, basename(bin_files[0][:-4]) + '_' + basename(bin_files[1][:-4]) + '.hdf5')
+    if make_test:
+        # for a shorter file for suite2p training, modify the outpath and number of processed frames - MM 17 Oct 2019
+        out_path = join(folders, basename(bin_files[0][:-4]) + '_' + basename(bin_files[1][:-4]) + '_test.hdf5')
+    else:
+        out_path = join(folders, basename(bin_files[0][:-4]) + '_' + basename(bin_files[1][:-4]) + '.hdf5')
+
+    # So you remember which file you just processed - MM 17 Oct 2019
+    print(out_path)
 
     # read them, reformat and write
     with open(green_path, 'rb') as g, open(red_path, 'rb') as r, h5py.File(out_path, 'w') as d:
@@ -31,13 +44,28 @@ for folders in file_path:
         file_size -= g.tell()
         # get the number of frames
         nbr_frames = np.round(file_size/(x_res*y_res*2)).astype('int32')
+        # for a shorter file for suite2p training, modify the outpath and number of processed frames - MM 17 Oct 2019
+        if make_test:
+            out_path = join(folders, basename(bin_files[0][:-4]) + '_' + basename(bin_files[1][:-4]) + '_test.hdf5')
+            nbr_frames = int(nbr_frames*0.2)
         # set the position of the red file also at the beginning of the frame
         r.seek(4, 0)
         # create the hdf5 dataset
         out = d.create_dataset('data', (nbr_frames*2, x_res, y_res), dtype='int16')
+        # Set some variables for a progress bar - MM 17 Oct 2019
+        point = nbr_frames // 100
+        increment = nbr_frames // 20
 
         # for all the frames
         for index, frames in enumerate(range(nbr_frames)):
+
+            # Provide a progress bar every 5% of total frames converted
+            if index % (5*point) == 0:
+                num_eqs = index // increment
+                num_spaces = (nbr_frames-index)//increment
+                percentage = index//point
+                print("\r[" + "=" * num_eqs + " " * num_spaces + "] " + str(percentage) + "%")
+
             # load the data
             data_green = np.fromfile(g, 'int16', count=x_res * y_res)
             data_red = np.fromfile(r, 'int16', count=x_res * y_res)
@@ -47,5 +75,3 @@ for folders in file_path:
             # save in the hdf5
             out[index*2, :, :] = data_green
             out[(index * 2) + 1, :, :] = data_red
-
-
